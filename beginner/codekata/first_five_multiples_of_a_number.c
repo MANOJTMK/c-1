0c5dcfbdd9d7f25f7b955e7afe0ea1710a4e5c25
Prathika_S <34652297+Prathika-S@users.noreply.github.com>
2018-01-02 21:46:38 +0530

    Add files via upload

---
+++ beginner/codekata/first_five_multiples_of_a_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+int i,n,p;
+
+scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+
+{
+
+p=n*i;
+
+printf("%d\t",p);
+
+}
+	return 0;
+
+}
---
+++ beginner/codekata/number_is_prime_or_not.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+
+  int i,a,c=0;
+
+  scanf("%d",&a);
+
+  for(i=2;i<a;i++)
+
+  {
+
+     if(a%i==0)
+
+     {
+
+        c++;
+
+      }
+
+   }
+
+  if(c==0)
+
+  {
+
+  printf("Prime number");
+
+  }
+
+  else
+
+  {
+
+  printf("Not a prime number");
+
+  }
+
+  return 0;
+
+}
