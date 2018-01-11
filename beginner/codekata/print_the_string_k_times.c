8671c0adcd1ebd71100a66ac3cb0c3ee0faf51b2
Prathika_S <34652297+Prathika-S@users.noreply.github.com>
2018-01-11 21:16:08 +0530

    Add files via upload

---
+++ beginner/codekata/print_the_string_k_times.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[50];
+    int i,n;
+    scanf("%s",s);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%s\n",s);
+    }
+    return 0;
+}
