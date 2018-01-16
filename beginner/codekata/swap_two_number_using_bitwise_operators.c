b1400bf6dd608ade163e2fa492f7570879d0774b
Prathika_S <34652297+Prathika-S@users.noreply.github.com>
2018-01-16 14:05:15 +0530

    Update swap_two_number_using_bitwise_operators.c

--- beginner/codekata/swap_two_number_using_bitwise_operators.c
+++ beginner/codekata/swap_two_number_using_bitwise_operators.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-  int n1, n2;
-  scanf(" %d \t  %d\n", &n1, &n2);
-  n1 = n1 ^ n2;
-  n2 = n1 ^ n2;
-  n1 = n1 ^ n2;
-  printf(" %d \t %d\n", n1, n2);
+  int n11, n2;
+  scanf(" %d \t  %d\n", &n11, &n2);
+  n11 = n11 ^ n2;
+  n2 = n11 ^ n2;
+  n11 = n11 ^ n2;
+  printf(" %d \t %d\n", n11, n2);
   return 0;
 }
