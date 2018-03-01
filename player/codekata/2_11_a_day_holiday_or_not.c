6e1e5caab77bd229b5d841dadad1e78fba11db14
Prathika_S <34652297+Prathika-S@users.noreply.github.com>
2018-03-01 21:12:16 +0530

    Add files via upload

---
+++ player/codekata/2_11_a_day_holiday_or_not.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) 
+
+{
+
+	char str[55];
+
+	scanf("%s",str);
+
+	if(str[0]=='S' || str[0]=='s')
+
+	{
+
+		printf("yes");
+
+	}
+
+	else
+
+	{
+
+		printf("no");
+
+	}
+
+	return 0;
+
+}
