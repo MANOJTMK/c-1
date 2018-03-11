bf7bce018d6b856129691e60d4b0a2b195927dc7
Prathika_S <34652297+Prathika-S@users.noreply.github.com>
2018-03-12 00:30:35 +0530

    Add files via upload

---
+++ player/codekata/5_44_print_the_rotated_string_by_k_positions.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+#include<string.h>
+
+void main()
+
+{int i,j=0,n;
+
+char a[10000],b[10000];
+
+	scanf("%s",a);
+
+	scanf("%d",&n);
+
+	if(n>strlen(a))
+
+	{
+
+		printf("%s",a);
+
+	}
+
+	for(i=n;i<=strlen(a);i++)
+
+	{
+
+		b[j]=a[i];
+
+		j++;
+
+	}	
+
+			j=strlen(a)-n;
+
+			for(i=0;i<n;i++)
+
+			{
+
+				b[j]=a[i];
+
+				j++;
+
+			}
+
+				printf("%s",b);
+
+		
+
+}
---
+++ player/codekata/6_53_length_by_strlen.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+
+	char a[15];
+
+	int i,flag;
+
+	scanf("%s",a);
+
+	for(i=0;a[i]!='\0';i++)
+
+	{
+
+		flag=0;	
+
+	}
+
+	printf("%d",i);
+
+	return 0;
+
+}
